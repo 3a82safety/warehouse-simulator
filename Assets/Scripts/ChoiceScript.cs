bbf9837b206d9823d83283df8832e4cdbe2b5136
Dexter825 <DEXTER.21@ichat.sp.edu.sg>
2023-05-08 08:59:20 +0800

    Resolved Merge Conflict

---
+++ Assets/Scripts/ChoiceScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceScript : MonoBehaviour
+
+    public GameObject Textbox;
+    public GameObject Choice1;
+    public GameObject Choice2;
+    public GameObject Choice3;
+    public GameObject Choice4;\
+    public int ChoiceMade;
+
+    public void ChoiceOption1
+
+    void Update()
+    {
+        
+    }
+}
