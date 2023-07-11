bfff50adb220324d503f4a6c45a844099b804e85
Dexter825 <DEXTER.21@ichat.sp.edu.sg>
2023-07-11 16:56:44 +0800

    Updated Assessment Scene

--- Assets/Scripts/InstantiateRandomly.cs
+++ Assets/Scripts/InstantiateRandomly.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class InstantiateRandomly : MonoBehaviour
 {
     public Transform pos;
 
